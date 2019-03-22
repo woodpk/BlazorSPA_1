7ce5cbf6d91cd69630854bb38a53b80b1c022b40
Patrick Wood <woodpk@outlook.com>
2019-03-22 15:58:00 -0400

    Initial app creation

---
+++ SPA_Core3_7/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace SPA_Core3_7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
